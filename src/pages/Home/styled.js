c0f8b1346f4f9fd3ceec6a2d81984f55eb5a8a32
Luiz Assis <luizhsg7@gmail.com>
2021-07-20 15:03:06 -0300

    primeiro commit

---
+++ src/pages/Home/styled.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`;
+
+export const Content = styled.div`
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Input = styled.input`
+    border: 1px solid #DDD;
+    height: 1.5rem;
+    padding: 0 .5rem;
+    border-radius: .25rem 0 0 .25rem;
+
+    &:focus,
+    &:active {
+        outline: none;
+        box-shadow: none;
+    };
+`;
+
+export const Button = styled.button`
+    height: 1.5rem;
+    border: 1px solid #000;
+    background-color: #000;
+    color: #FFF;
+    border-radius: 0 .25rem .25rem 0;
+
+    &:focus,
+    &:active,
+    &:hover {
+        outline: none;
+        box-shadow: none;
+        cursor: pointer;
+    };
+`;
+
+export const ErrorMsg = styled.span`
+    display: block;
+    font-size: .65rem;
+    color: red;
+    font-weight: 600;
+    margin-top: 1rem;
+`;
